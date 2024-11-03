323b963cd89e2ff75e0feebfd8eda7544be4e1f3
Aman <72264622+amandev8207@users.noreply.github.com>
2024-11-03 16:08:09 +0000

    add daily challenges

---
+++ day-14.cpp
@@ -0,0 +1,16 @@
+// #796. Rotate String
+
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if (s.length() != goal.length()) {
+//             return false;
+//         }
+//         return (s + s).find(goal) != string::npos;
+//     }
+// };
