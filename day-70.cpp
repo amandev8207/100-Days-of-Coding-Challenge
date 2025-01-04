f34bc70b72ee877bd0099a3c1006983fe6bb1f72
Aman <72264622+amandev8207@users.noreply.github.com>
2025-01-04 15:22:55 +0000

    add daily challenges

---
+++ day-70.cpp
@@ -0,0 +1,34 @@
+// 1930. Unique Length-3 Palindromic Subsequences
+// Medium
+// Topics
+// Companies
+// Hint
+// Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+// Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+// A palindrome is a string that reads the same forwards and backwards.
+
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+// For example, "ace" is a subsequence of "abcde".
+
+
+
+
+
+
+// 1930. Unique Length-3 Palindromic Subsequences
+// Medium
+// Topics
+// Companies
+// Hint
+// Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+// Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+// A palindrome is a string that reads the same forwards and backwards.
+
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+// For example, "ace" is a subsequence of "abcde".
