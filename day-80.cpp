70154583200b1dc4f81e826029854f5e0474f28d
Aman <72264622+amandev8207@users.noreply.github.com>
2025-01-15 15:48:58 +0000

    add daily challenges

--- day-80.cpp
+++ day-80.cpp
@@ -1,31 +1,31 @@
-#2657. Find the Prefix Common Array of Two Arrays
+// #2657. Find the Prefix Common Array of Two Arrays
 
-You are given two 0-indexed integer permutations A and B of length n.
+// You are given two 0-indexed integer permutations A and B of length n.
 
-A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
+// A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
 
-Return the prefix common array of A and B.
+// Return the prefix common array of A and B.
 
-A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
-
-
+// A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
 
 
 
-class Solution {
-public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        unordered_set<int> seen; 
-        vector<int> C(n, 0);
-        int commonCount{0};
-        for (int i{0}; i < n; ++i) {
-            if (seen.count(A[i])) ++commonCount;
-            else seen.insert(A[i]);
-            if (seen.count(B[i])) ++commonCount;
-            else seen.insert(B[i]);
-            C[i] = commonCount;
-        }
-        return C;
-    }
-};
+
+
+// class Solution {
+// public:
+//     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+//         int n = A.size();
+//         unordered_set<int> seen; 
+//         vector<int> C(n, 0);
+//         int commonCount{0};
+//         for (int i{0}; i < n; ++i) {
+//             if (seen.count(A[i])) ++commonCount;
+//             else seen.insert(A[i]);
+//             if (seen.count(B[i])) ++commonCount;
+//             else seen.insert(B[i]);
+//             C[i] = commonCount;
+//         }
+//         return C;
+//     }
+// };
